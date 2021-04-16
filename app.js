7be1466cc4c5e26e9c348a79885ad04a96c8a238
Anida Mujezin <anidam@comtrade.com>
2021-04-16 13:12:28 +0200

    m

--- app.js
+++ app.js
@@ -55,8 +55,8 @@ app.use(function (req, res, next) {
 
 /* END MIDDLEWARE  */
 
-app.use("/api/users", usersRoutes);
-app.use("/api/posts", postsRoutes);
+app.use("/users", usersRoutes);
+app.use("/posts", postsRoutes);
 
 module.exports = app;
 
