6d420323d0c267d02b0e6c33bc2dfff9453947b7
Anida <54995191+anida21@users.noreply.github.com>
2021-05-25 12:57:31 +0200

    Add files via upload

--- client/src/app/web-request.service.ts
+++ client/src/app/web-request.service.ts
@@ -10,31 +10,31 @@ export class WebRequestService {
 
   constructor(private http: HttpClient) {
     
-    this.ROOT_URL = '';
+    this.ROOT_URL = 'https://sarajevotravel.herokuapp.com';
     
   }
 
   get(uri: string) {
-    return this.http.get(`${this.ROOT_URL}/${uri}`);
+    return this.http.get(`https://sarajevotravel.herokuapp.com/${uri}`);
   }
 
   post(uri: string, payload: Object) {
-    const res = this.http.post(`${this.ROOT_URL}/${uri}`, payload);
+    const res = this.http.post(`https://sarajevotravel.herokuapp.com/${uri}`, payload);
     console.log(res);
     return res; //this.http.post(`${this.ROOT_URL}/${uri}`, payload);
   }
 
   patch(uri: string, payload: Object) {
-    return this.http.patch(`${this.ROOT_URL}/${uri}`, payload);
+    return this.http.patch(`https://sarajevotravel.herokuapp.com/${uri}`, payload);
   }
 
   delete(uri: string) {
-    return this.http.delete(`${this.ROOT_URL}/${uri}`);
+    return this.http.delete(`https://sarajevotravel.herokuapp.com/${uri}`);
   }
 
   login(email: string, password: string) {
     return this.http.post(
-      `${this.ROOT_URL}/api/users/login`,
+      `$https://sarajevotravel.herokuapp.com/api/users/login`,
       {
         email,
         password,
@@ -47,7 +47,7 @@ export class WebRequestService {
 
   signup(email: string, password: string, username: string) {
     return this.http.post(
-      `${this.ROOT_URL}/api/users`,
+      `https://sarajevotravel.herokuapp.com/api/users`,
       {
         email,
         password,
